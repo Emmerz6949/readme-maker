dce01aa31f582825a57d18f46ae59388205c13c1
emmerz6949 <emmerz6949@gmail.com>
2020-07-22 01:51:18 -0400

    installed inquirer, created the prompt questions, and added an object destructuring assignment for the variable info

--- index.js
+++ index.js
@@ -4,3 +4,55 @@ const inquirer = require("inquirer");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
+generateReadMe ();
+
+async function generateReadMe() {
+    try {
+        const info = await inquirer.prompt([
+        {
+            message: "Please enter the title of your project:",
+            name: "title"
+        },
+        {
+            message: "Please enter a description for your project:",
+            name: "description"
+        },
+        {
+            message: "Please enter installation instructions:",
+            name: "install"
+        },
+        {
+            message: "Please enter usage information:",
+            name: "usage"
+        },
+        {
+            message: "Please enter contribution guidelines:",
+            name: "contribute"
+        },
+        {
+            message: "Please enter test instructions:",
+            name: "test"
+        },
+        {
+            message: "Please enter your GitHub username:",
+            name: "hubName"
+        },
+        {
+            message: "Please enter your email:",
+            name: "email"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Please choose a licenese:",
+            choices: ["Apache License 2.0", "MIT License", "None"]
+        }
+    ]);
+    console.log(info);
+
+    const {title, description, install, usage, contribute, test, hubName, email, license} = info;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
