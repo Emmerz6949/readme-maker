c599826559f4e6bb24162ac15dbbd64986a59921
emmerz6949 <emmerz6949@gmail.com>
2020-07-24 18:16:47 -0400

    added a writeFileAsync function call to write the userREADME.md using the user input gathered by the prompts

--- index.js
+++ index.js
@@ -45,12 +45,64 @@ async function generateReadMe() {
             type: "list",
             name: "license",
             message: "Please choose a licenese:",
-            choices: ["Apache License 2.0", "MIT License", "None"]
+            choices: ["Apache2.0", "MIT", "GPL", "None"]
         }
     ]);
     console.log(info);
 
     const {title, description, install, usage, contribute, test, hubName, email, license} = info;
+
+    await writeFileAsync("userREADME.md", 
+`# ${title}  &nbsp;&nbsp;&nbsp; ![image](https://img.shields.io/badge/license-${license}-blueviolet)
+    
+## Description
+
+${description}
+
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
+
+
+## Installation
+
+${install}
+
+
+## Usage
+
+${usage}
+
+
+## Contributing
+
+${contribute}
+
+
+## Tests
+
+${test}
+
+
+## Questions
+
+If you have any questions feel free to reach out to me on my GitHub page or via e-mail!
+
+${hubName}: https://github.com/${hubName}
+
+E-mail: ${email}
+
+
+## License
+
+${license}
+    `);
     }
     catch (err) {
         console.log(err);
